c4c322a2d1c4e676b29e5028d6145b2be1ddc66c
yakovevsasha1998-crypto <yakovevsasha1998@gmail.com>
2026-04-03 17:35:32 +0300

    Сделал пол дела в schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,25 @@
 from pydantic import BaseModel,Field,EmailStr,SecretStr
 
+#создаем пользователя
 class UserCreate(BaseModel):
+    username:str = Field(min_length=2,max_length=100)
+    age:int = Field(ge=0,le=100)
+    password:SecretStr
+    bio:str|None = None
+
+#вход в аккаунт  
+class UserLogin(BaseModel):
+    username:str = Field(min_length=2,max_length=100)
+    password:SecretStr
+    
+#сздание задачи
+
+class CreateTask(BaseModel):
+    name_task:str 
+    description:str
+    
+
+    
+
     
     
